241bf06c7516828342fc205c760def609d382be6
Johny Blaze <johny.urgiles@gmail.com>
2012-02-25 18:49:35 -0500

    adding basic lastfm api call

---
+++ js/lastfm.js
@@ -0,0 +1,19 @@
+function getLastFmTopCountrySongs(countryname){
+    var api_key = "3938d8cf503b62fcc4d3c616d2f99b48";
+    var req = new XMLHttpRequest();
+    var reqUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=ecuador&api_key="+api_key;
+    console.log("Request: " + reqUrl);
+    req.open("GET", reqUrl, false);
+    console.log("Request opened");
+    req.onreadystatechange = function() {
+        console.log("req status: " + req.status);
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                console.log("Search complete!");
+                console.log(req.responseText);
+            }
+        }
+    };
+    req.send();
+    console.log("Request sent");
+}   
--- tutorial.js
+++ tutorial.js
@@ -35,4 +35,4 @@ function init() {
     });
 
     tl.run();
-}   
+}
