9ff9eca50aca9471b35ecf44cd2a66b301c5d057
Johny Blaze <johny.urgiles@gmail.com>
2012-02-25 02:43:36 -0500

    add basic tracklist

---
+++ tutorial.js
@@ -0,0 +1,26 @@
+sp = getSpotifyApi(1);
+
+var models = sp.require('sp://import/scripts/api/models');
+var views = sp.require('sp://import/scripts/api/views');
+
+exports.init = init;
+
+function init() {
+    console.log("woohoo");
+    var pl = new models.Playlist();
+    var tracks = models.library.tracks;
+    
+    for (var i=0;i<20;i++){
+        var track = models.Track.fromURI(tracks[i].data.uri);
+        pl.add(track);
+    }
+
+    var player = new views.Player();
+    player.track = pl.get(0);
+    player.context = pl;
+   
+    $('#player').append(player.node);
+
+    var plView = new views.List(pl);
+    $('#player').append(plView.node);
+}   
