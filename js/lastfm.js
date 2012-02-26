c509832831792585c163e820e9213e147d6634d7
Johny Blaze <johny.urgiles@gmail.com>
2012-02-25 21:21:46 -0500

    fixing live fm url

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,7 +1,7 @@
 function getLastFmTopCountrySongs(countryName){
     var api_key = "3938d8cf503b62fcc4d3c616d2f99b48";
     var req = new XMLHttpRequest();
-    var reqUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=" + countryName + " + &api_key="+api_key;
+    var reqUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=" + countryName + "&api_key="+api_key;
     console.log("Request: " + reqUrl);
     req.open("GET", reqUrl, false);
     req.onreadystatechange = function() {
@@ -10,8 +10,8 @@ function getLastFmTopCountrySongs(countryName){
             if (req.status == 200) {
                 r = req.responseText;
                 $('track', r).each(function(){
-						// console.log($(this));
-                   console.log($(this).attr("name"));//  + " " + $(this).attr("artist").attr("name"));
+                   console.log($(this).attr('name'));
+                  // console.log($(this).attr("name"));//  + " " + $(this).attr("artist").attr("name"));
                 });
             }
         }
