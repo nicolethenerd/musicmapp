db1b0de773e183501b90ee614610a80e89df66e1
Johny Blaze <johny.urgiles@gmail.com>
2012-02-25 22:17:19 -0500

    parse last fm with jquery

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,20 +1,14 @@
-function getLastFmTopCountrySongs(countryName){
+function getLastFmTopTrack(countryName){
     var api_key = "3938d8cf503b62fcc4d3c616d2f99b48";
-    var req = new XMLHttpRequest();
     var reqUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=" + countryName + "&api_key="+api_key;
     console.log("Request: " + reqUrl);
-    req.open("GET", reqUrl, false);
-    req.onreadystatechange = function() {
-        console.log("req status: " + req.status);
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                r = req.responseText;
-                $('track', r).each(function(){
-                   console.log($(this).attr('name'));
-                  // console.log($(this).attr("name"));//  + " " + $(this).attr("artist").attr("name"));
+    $.ajax({
+    url: reqUrl,
+    dataType: "xml",
+    success: function(xml){
+               $('track', xml).each(function(){
+                   console.log( $(this).find('name').text() + " -  " +  $(this).find('artist').find('name').text());
                 });
-            }
-        }
-    };
-    req.send();
-}   
+    }
+ });
+}
